3cbc47ca31d98d307e1ddd0f8f33218af035b234
anushka192001 <63954873+anushka192001@users.noreply.github.com>
2022-01-21 04:08:36 +0530

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -26,9 +26,6 @@ figures_path="figures"
 # Y axis points list  in  average_sentimental_score vs year
 avg_y=[]
 
-#initializing fig and axes to plot 1 graph containing average_sentimental_score vs year
-fig, axes = plt.subplots(1,1) 
-
 #iterate over years 1995 to 2021
 for yr in f1.years:
       
@@ -50,11 +47,15 @@ for yr in f1.years:
           base_name=f'{i}.txt'   
           
            #open the 8-k file to read 
-          f=open('/'.join([f1.fillings_path,str(yr),str(qtr),base_name]), 'r')
+          f=open('/'.join([f1.fillings_path,str(yr),str(qtr),base_name]), 'r+')
           
           #extract only text of the 8-k filling(filtering tags in it)(clean the file data)
           Ctext = BeautifulSoup(f, features="lxml").get_text()
-          
+      
+          #rewrite the clean text on the file
+          f.write(Ctext)
+          f.close()  
+            
           #convert the extracted text to lowercase
           Ctext=Ctext.lower()
           
@@ -101,30 +102,40 @@ for yr in f1.years:
         #close the csv file   
         file_csv.close()    
         
-        #sort the quaterly_y(sentimental_scores) in the same order when we sort quaterly_x(filling dates)  to preserve the key value pair
-        quoterly_y=[x for _, x in sorted(zip(quatorly_x,quatorly_y))]
-        quatorly_x=sorted(quatorly_x)
         
-       #now append the mean of the quaterly_y list to the yearly_y list
-        yearly_y.append(statistics.mean(quatorly_y))
         
+        #if path to the file does not exist   
+        if not os.path.exists('/'.join([figures_path,str(yr)])):      
+         #sort the quaterly_y(sentimental_scores) in the same order when we sort quaterly_x(filling dates)  to preserve the key value pair
+          quoterly_y=[x for _, x in sorted(zip(quatorly_x,quatorly_y))]
+          quatorly_x=sorted(quatorly_x) 
         #subplot a graph of sentimental_score vs quarter in each year
-        axes1[pyplot_ind[k][0],pyplot_ind[k][1]].set_title(qtr, fontsize=14) 
-        axes1[pyplot_ind[k][0],pyplot_ind[k][1]].set_xlabel('date', fontsize=10)
-        axes1[pyplot_ind[k][0],pyplot_ind[k][1]].set_ylabel('good_sentiment score', fontsize=10)
-        axes1[pyplot_ind[k][0],pyplot_ind[k][1]].plot(quatorly_x,quatorly_y)  
-        axes1[pyplot_ind[k][0],pyplot_ind[k][1]].set_xticklabels(quatorly_x,rotation=90)
-        
-        #create a path (for ex: "figures/1995") and save the figure (plot of sentimental_score of 4 quarters in 1 figure) there
-      os.makedirs('/'.join([figures_path,str(yr)]))  
-      fig1.savefig('/'.join([figures_path,str(yr),'plot.png']))   
-        
+          axes1[pyplot_ind[k][0],pyplot_ind[k][1]].set_title(qtr, fontsize=14) 
+          axes1[pyplot_ind[k][0],pyplot_ind[k][1]].set_xlabel('date', fontsize=10)
+          axes1[pyplot_ind[k][0],pyplot_ind[k][1]].set_ylabel('good_sentiment score', fontsize=10)
+          axes1[pyplot_ind[k][0],pyplot_ind[k][1]].plot(quatorly_x,quatorly_y)  
+          axes1[pyplot_ind[k][0],pyplot_ind[k][1]].set_xticklabels(quatorly_x,rotation=90)
+      
+         #now append the mean of the quaterly_y list to the yearly_y list
+        yearly_y.append(statistics.mean(quatorly_y)) 
+      
+      #if path to the file does not exist   
+      if not os.path.exists('/'.join([figures_path,str(yr)])):  
+      #create a path (for ex: "figures/1995") and save the figure (plot of sentimental_score of 4 quarters in 1 figure) there
+        os.makedirs('/'.join([figures_path,str(yr)]))  
+        fig1.savefig('/'.join([figures_path,str(yr),'plot.png']))   
+        plt.close()
+            
     #append the mean of the yearly_y list to the avg_y list
       avg_y.append(statistics.mean(yearly_y))   
-      
- #subplot a graph of sentimental_score vs year     
+ 
+
+ #plot a graph of sentimental_score vs year 
+fig, axes = plt.subplots(1,1) 
 axes[0,0].set_title("yearwise distribution of sentiment score", fontsize=18)
 axes[0,0].set_xlabel('year', fontsize=18)
 axes[0,0].set_ylabel('good_sentiment score', fontsize=18)
+axes[0,0].plot(quatorly_x,quatorly_y)  
 plt.xticks(rotation=90)  
 fig.savefig('figures/year_vs_sentiment_score_plot.png')
+plt.close()
